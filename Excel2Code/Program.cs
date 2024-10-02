c28ffda3d49592dbe76146ec253f492065027a99
FS <yukuyoulei@hotmail.com>
2024-10-02 09:49:54 +0800

    add ignore argument

--- Excel2Code/Program.cs
+++ Excel2Code/Program.cs
@@ -1,31 +1,40 @@
-﻿using System;
+using System;
 using System.IO;
 
 namespace Excel2Code
 {
-	class Program
-	{
-		static void Main(string[] args)
-		{
-			if (!Directory.Exists("Codes"))
-				Directory.CreateDirectory("Codes");
-			if (args.Length == 0)
-			{
-				Console.WriteLine("Input a dir:");
-				var dir = Console.ReadLine();
-				Excel2Code.GenerateFromDir(dir);
-			}
-			else if (args[0] == "-dir")
-			{
-				Excel2Code.GenerateFromDir(args[1]);
-			}
-			else
-				foreach (var f in args)
-				{
-					Excel2Code.Generate(f);
-				}
-			Console.WriteLine("Press any key to exit...");
-			Console.ReadKey();
-		}
-	}
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (!Directory.Exists("Codes"))
+                Directory.CreateDirectory("Codes");
+            string[] ignores = null;
+            for (int i = 0; i < args.Length; i++)
+            {
+                if (args[i] == "ignore")
+                {
+                    ignores = args[++i].Split(',');
+                    break;
+                }
+            }
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Input a dir:");
+                var dir = Console.ReadLine();
+                Excel2Code.GenerateFromDir(dir, ignores);
+            }
+            else if (args[0] == "-dir")
+            {
+                Excel2Code.GenerateFromDir(args[1], ignores);
+            }
+            else
+                foreach (var f in args)
+                {
+                    Excel2Code.Generate(f);
+                }
+            //Console.WriteLine("Press any key to exit...");
+            //Console.ReadKey();
+        }
+    }
 }
